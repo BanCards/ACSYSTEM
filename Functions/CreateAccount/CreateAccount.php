4f56eb96d8cc97ae2d44c317ef1425f95b0e427c
BanCards <bancardsme@gmail.com>
2023-10-25 11:08:16 +0900

    [modify] スペルミス

--- Functions/CreateAccount/CreateAccount.php
+++ Functions/CreateAccount/CreateAccount.php
@@ -34,8 +34,8 @@ function isEmpty($value)
     <title>ACSystem - Register Account</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" href="../../Css/Common.css">
-    <link rel="stylesheet" href="Css/CreateAccount.css">
+    <link rel="stylesheet" href="../../CSS/Common.css">
+    <link rel="stylesheet" href="CSS/CreateAccount.css">
 </head>
 
 <body>
