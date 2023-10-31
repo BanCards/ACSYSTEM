8cff66bd0c792dc1859fe327d8e89745f690fa3b
BanCards <bancardsme@gmail.com>
2023-10-31 13:30:40 +0900

    [delete] 不要な部分の削除

--- Functions/CreateAccount/CreateAccount.php
+++ Functions/CreateAccount/CreateAccount.php
@@ -1,29 +1,8 @@
 <?php
 session_status() == PHP_SESSION_NONE ? session_start() : sleep(0);
 
-//tempのカードIDはセッション間で一旦渡しあう
+//tempのカードIDはセッション間で一旦渡す
 $_SESSION['cardID'] = $_POST['cardID'];
-
-//セッション間でエラー系の情報引き渡す
-function setError($errorTitle, $errorMessage, $errorCode)
-{
-    $_SESSION['errorTitle'] = $errorTitle;
-    $_SESSION['errorMessage'] = $errorMessage;
-    $_SESSION['errorCode'] = "エラーコード : " . $errorCode . date("ymdis");
-    header('Location:LoadInformationError.php');
-    return;
-}
-
-//引数の値がnullならsetError()を行う。　存在するならfalseを返す。
-function isEmpty($value)
-{
-    if (empty($value)) {
-        setError("情報エラー", "ACSystemチームまでご連絡ください。", "12A_");
-        return true;
-    }
-
-    return false;
-}
 ?>
 
 <!DOCTYPE html>
