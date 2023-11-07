104eb69113d45f6f9ee2551ba1efd3e1c534526d
BanCards <bancardsme@gmail.com>
2023-11-07 11:03:51 +0900

    [add] 変更履歴をtxtに追加したいが、現在は保留

--- Functions/Profile/EditProfile/Events/LoadInformation.php
+++ Functions/Profile/EditProfile/Events/LoadInformation.php
@@ -3,12 +3,11 @@ include('../../../Utils/Utils.php');
 session_status() == PHP_SESSION_NONE ? session_start() : sleep(0);
 
 $key = str_replace('_info', '', $_SESSION['editItem']);
-$oldValue = $_POST['current-item-value'];
-$newValue = $_POST['new-item-value'];
+$value = $_POST['new-item-value'];
 
 unset($_SESSION['editItem']);
 
-if (empty($newValue) || empty($oldValue)) {
+if (empty($value)) {
     setError("記入されていない欄があります。", "もう一度記入されているか確認してください。", "12I_");
     header("Location: ../LoadInformationError.php");
     return;
@@ -26,33 +25,28 @@ if ($pdo == null) {
 }
 
 $query = "SELECT card_id, class, name, email, password FROM users WHERE id = ?";
-$stmt = $conn->prepare($query);
-$stmt->bindValue(':uuid', $uuid, PDO::PARAM_STR);
+$stmt = $pdo->prepare($query);
+$stmt->bindValue(1, $uuid, PDO::PARAM_STR);
 $stmt->execute();
 $result = $stmt->fetch();
 
 if (!$result) {
     setError("情報エラー", "ACSystemチームまでご連絡ください。", "21D_");
-
     return;
 }
 
-$outputs_directory = "../../../../Outputs/";
+$outputs_directory = '../Outputs/';
 $filename = date("Y/m/d") . "_変更履歴.txt";
-$message = getUserName() . "が" . $key . "を" . $result[$key] . "から更新しました  :  " . $newValue;
+$message = getUserName() . "が" . $key . "を" . $result[$key] . "から更新しました  :  " . $value;
 
-if (!is_dir($outputs_directory)) {
-    mkdir($outputs_directory, 0777, true);
-}
-
-
-if (file_exists($txt_filename)) {
-    $existing_data = file_get_contents($txt_filename);
+// 既存のファイルにメッセージを追加
+if (file_exists($filename)) {
+    $existing_data = file_get_contents($filename);
     $existing_data .= "\n" . $message;
 } else {
     $existing_data = $message;
 }
 
-file_put_contents($txt_filename, $existing_data);
+file_put_contents($filename, $existing_data);
 
 header("Location: http://localhost/ACSystem/Functions/Profile/EditProfile/SelectEditItem.php");
