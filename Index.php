6889eccd6e2c6b081371464fc6e5e0c3e92047e5
BanCards <bancardsme@gmail.com>
2023-11-14 12:30:57 +0900

    [modify] ボタンのデザインを変更

--- Index.php
+++ Index.php
@@ -27,19 +27,22 @@ session_status() == PHP_SESSION_NONE ? session_start() : sleep(0);
       <div class="item">
         <a href="Functions/Attendance/WebAttendance/WebAttendance.php" class="btn btn--circle" id="report">
           <i class="fas fa-user-alt-slash"></i>
-          <br>申請をする
+          <br>
+          <p>申請をする</p>
           <i class="fas fa-angle-down fa-position-bottom"></i>
         </a>
 
         <a href="Functions/Profile/Record.php" class="btn btn--circle" id="record">
           <i class="fas fa-calendar-week"></i>
-          <br>履歴を見る
+          <br>
+          <p>履歴を見る</p>
           <i class="fas fa-angle-down fa-position-bottom"></i>
         </a>
 
         <a href="Functions/Attendance/DirectAttendance/DirectAttendance.php" class="btn btn--circle" id="test">
           <i class="fas fa-id-card"></i>
-          <br>βテスト
+          <br>
+          <p>βテスト</p>
           <i class="fas fa-angle-down fa-position-bottom"></i>
         </a>
       </div>
