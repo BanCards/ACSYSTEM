272341c7663bb4e7c0832c4575e14d7868f368a4
BanCards <bancardsme@gmail.com>
2023-11-16 12:10:00 +0900

    [add] お問い合わせ内容をtxt形式で書き込むようにした

--- Functions/Contact/Events/LoadContact.php
+++ Functions/Contact/Events/LoadContact.php
@@ -2,5 +2,27 @@
 include('../../Utils/Utils.php');
 session_status() == PHP_SESSION_NONE ? session_start() : sleep(0);
 
-echo $_SESSION['contactTitle'];
-echo $_SESSION['contactContents'];
+if (!(isLoggedIn())) {
+    setError("ログイン情報エラー", "ログインしてください。", "12A");
+    return;
+}
+
+isEmptyItems($_SESSION['contactTitle'], $_SESSION['contactContents']);
+
+$time = DateTime::createFromFormat("Y-m-d H:i:s", getCurrentTime())->format("Ymd");
+$file = '../Contacts/' . $time . ".txt";
+
+$title =  $_SESSION['contactTitle'];
+$messages =  $_SESSION['contactContents'];
+
+$contents = "\n[From] " . getUserName() . " - " . getUserEmail() . "\n[Time] " . getCurrentTime() . "\n[Title] " . $title . "\n[Messages] " . $messages . "\n";
+
+if (!file_exists('data.txt')) {
+    file_put_contents($file, $contents, FILE_APPEND | LOCK_EX);
+} else {
+    $fp = fopen($file, 'w');
+    fputs($fp, $contents);
+    fclose($fp);
+}
+
+setSuccess("内容を送信しました");
