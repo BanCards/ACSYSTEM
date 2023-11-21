be977baba34aaed07d0c8a37bdb5c5b030d7a8e8
BanCards <bancardsme@gmail.com>
2023-11-21 15:46:29 +0900

    [modify] タイトル変更

--- Functions/Response/LoadInformationError.php
+++ Functions/Response/LoadInformationError.php
@@ -7,7 +7,7 @@ session_status() == PHP_SESSION_NONE ? session_start() : sleep(0);
 <html lang="ja">
 
 <head>
-    <title>ACSystem - Successful</title>
+    <title>ACSystem - Error</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="../../CSS/Common.css">
