3181bf7d0acb640d2c812b7836c9c8d62bf99d25
BanCards <bancardsme@gmail.com>
2023-11-22 10:33:28 +0900

    [add] setUserRecord()を追加

--- Functions/Attendance/DirectAttendance/Events/LoadInformation.php
+++ Functions/Attendance/DirectAttendance/Events/LoadInformation.php
@@ -5,7 +5,7 @@ session_status() == PHP_SESSION_NONE ? session_start() : sleep(0);
 $cardID = $_POST['cardID'];
 
 $pdo = getDatabaseConnection();
-if ($pdo == null) {
+if (!$pdo) {
     header("Location:../../LoadInformationError.php");
     return;
 }
@@ -17,28 +17,15 @@ $stmt->execute();
 
 $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+$pdo = null;
+
 if ($result) {
-    $uuid = $result['id'];
+    $user_id = $result['id'];
     $timestamp = getCurrentTime();
     $status = "attend";
     $comment = "カードによる出席";
 
-    $insertQuery = "INSERT INTO attendance (user_id, timestamp, status, comment) VALUES (:uuid, :timestamp, :status, :comment)";
-    $stmt = $pdo->prepare($insertQuery);
-
-    $stmt->bindParam(':uuid', $uuid, PDO::PARAM_INT);
-    $stmt->bindParam(':timestamp', $timestamp, PDO::PARAM_STR);
-    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
-    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
-
-
-    if ($stmt->execute()) {
-        setSuccess("出席処理が完了しました");
-        return;
-    } else {
-        setError("実行中にエラーが発生しました。", "ACSystemチームまでご連絡ください。", "13CA");
-        return;
-    }
+    setUserRecord($user_id, $timestamp, $status, $comment);
 } else {
     setError("ユーザーが見つかりませんでした。", "ACSystemチームまでご連絡ください。", "13U");
     return;
--- Functions/Attendance/WebAttendance/Events/LoadInformation.php
+++ Functions/Attendance/WebAttendance/Events/LoadInformation.php
@@ -2,46 +2,14 @@
 include('../../../Utils/Utils.php');
 session_status() == PHP_SESSION_NONE ? session_start() : sleep(0);
 
-if (empty(getLoginUUID())) {
+if (!(isLoggedIn())) {
     setError("ログイン情報エラー", "ログインしてください。", "12A");
-    return false;
+    return;
 }
 
-//値
+$user_id = getLoginUUID();
 $timestamp = getCurrentTime();
 $status = $_POST['status'];
 $comment = translate($_POST['reason']);
 
-$pdo = getDatabaseConnection();
-
-if ($pdo == null) return;
-
-$uuid = getLoginUUID();
-
-$query = "SELECT id FROM users WHERE id = :uuid";
-$stmt = $pdo->prepare($query);
-$stmt->bindParam(':uuid', $uuid, PDO::PARAM_INT);
-$stmt->execute();
-
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-if ($result) {
-    $insertQuery = "INSERT INTO attendance (user_id, timestamp, status, comment) VALUES (:uuid, :timestamp, :status, :comment)";
-    $stmt = $pdo->prepare($insertQuery);
-    $stmt->bindParam(':uuid', $uuid, PDO::PARAM_INT);
-    $stmt->bindParam(':timestamp', $timestamp, PDO::PARAM_STR);
-    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
-    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);
-
-
-    if ($stmt->execute()) {
-        setSuccess("処理が完了しました");
-        return;
-    } else {
-        setError("実行中にエラーが発生しました。", "ACSystemチームまでご連絡ください。", "13CA");
-        return;
-    }
-} else {
-    setError("ユーザーが見つかりませんでした。", "ACSystemチームまでご連絡ください。", "13U");
-    return;
-}
+setUserRecord($user_id, $timestamp, $status, $comment);
